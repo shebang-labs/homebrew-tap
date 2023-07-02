593c1bc06798debd3972c54ff2eab31d7ef32149
Tarek N. Elsamni <tarek.samni@gmail.com>
2023-07-02 22:09:23 +0100

    Update terraform-state-split.rb

--- terraform-state-split.rb
+++ terraform-state-split.rb
@@ -1,7 +1,4 @@
 class TerraformStateSplit < Formula
-  depends_on "ruby"
-  depends_on 'tty-prompt' => :ruby
-
   desc "This ruby CLI tool helps you organise large terraform state files into smaller ones by moving chosen resources from a provided state file into another destination state file."
   homepage "https://github.com/shebang-labs/terraform-state-split"
   url "https://github.com/shebang-labs/terraform-state-split/releases/download/1.0.0/terraform-state-split.tar.gz"
@@ -9,7 +6,7 @@ class TerraformStateSplit < Formula
   license "MIT"
 
   def install
-    #system "#{HOMEBREW_PREFIX}/opt/ruby/bin/gem install tty-prompt"
+    system "gem install tty-prompt"
     bin.install "terraform-state-split"
   end
 
