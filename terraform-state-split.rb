88f24583b104a521c692d4f8d32232861e6df0ca
Tarek N. Elsamni <tarek.samni@gmail.com>
2023-07-02 22:11:47 +0100

    Update terraform-state-split.rb

--- terraform-state-split.rb
+++ terraform-state-split.rb
@@ -1,5 +1,5 @@
 class TerraformStateSplit < Formula
-  depends_on :brew-gem
+  depends_on "brew-gem"
   desc "This ruby CLI tool helps you organise large terraform state files into smaller ones by moving chosen resources from a provided state file into another destination state file."
   homepage "https://github.com/shebang-labs/terraform-state-split"
   url "https://github.com/shebang-labs/terraform-state-split/releases/download/1.0.0/terraform-state-split.tar.gz"
