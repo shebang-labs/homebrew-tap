b6c2c6e2d0d899a6146ce41df08c0e48e4fca178
Sabhya <satyaguts1@proton.me>
2023-07-02 15:13:11 +0530

    Initial Commit

---
+++ terraform-state-split.rb
@@ -0,0 +1,16 @@
+class TerraformStateSplit < Formula
+  desc "This ruby CLI tool helps you organise large terraform state files into smaller ones by moving chosen resources from a provided state file into another destination state file."
+  homepage "https://github.com/shebang-labs/terraform-state-split"
+  url "https://github.com/shebang-labs/terraform-state-split/releases/download/1.0.0/terraform-state-split.tar.gz"
+  sha256 "010905a4d444dd4ffb4dc8ded7e239d52d678cd0d33873d468622cdbc3d71c69"
+  license "MIT"
+
+  def install
+    bin.install "terraform-state-split"
+  end
+
+  test do
+    system "#{bin}/terraform-state-split"
+  end
+end
+
