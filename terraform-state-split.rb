5e6881979bb0c3bac2e4003151cb298120de1749
Shebang Labs <hello@shebanglabs.io>
2023-07-03 15:55:07 +0000

    Update terraform-state-split.rb with new version

--- terraform-state-split.rb
+++ terraform-state-split.rb
@@ -1,9 +1,9 @@
 class TerraformStateSplit < Formula
   desc "This ruby CLI tool helps you organise large terraform state files into smaller ones by moving chosen resources from a provided state file into another destination state file."
   homepage "https://github.com/shebang-labs/terraform-state-split"
-  url "https://github.com/shebang-labs/terraform-state-split/releases/download/1.0.3/terraform-state-split.tar.gz"
+  url "https://github.com/shebang-labs/terraform-state-split/releases/download/1.0.0/terraform-state-split.tar.gz"
   license "MIT"
-  sha256 "3ebbad68a9ea10b8b78454b790fdb9682e2e26a57e9e290eb3dc6b6f47e0521f"
+  sha256 "893741038de59e37fcb267a3cf749943148a4434ad7b2e5043e269588b62dc53"
 
   uses_from_macos "ruby"
 
